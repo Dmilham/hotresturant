94fcb8be5214784c777208f948f56bab3d15a3df
Daniel Milham <dmilham462@gmail.com>
2016-10-29 11:37:14 -0400

    server.js update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@
 // Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
